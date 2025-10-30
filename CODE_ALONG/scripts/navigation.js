bf1b65d689e475606a07831d3c5c39f30ab2cb76
Christopher Bahinting <bahintingchristopher@gmail.com>
2025-10-30 10:19:27 +0800

    code along in header, navigation and footer

--- CODE_ALONG/scripts/navigation.js
+++ CODE_ALONG/scripts/navigation.js
@@ -1,8 +1,11 @@
 
 // store the selected elements that wer are going to use
 const navbutton = document.querySelector('#ham-btn');
+const navlinks = document.querySelector('#nav-bar');
 
 // toggle the show class OFF and ON
 navbutton.addEventListener('click', () => {
     navbutton.classList.toggle('show');
+    navlinks.classList.toggle('show');
+    
 })
